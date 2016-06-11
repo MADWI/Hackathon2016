d5b01519b0a176d655a3262534cdefbbb651419b
lkopocinski <lkopocinski@gmail.com>
2016-06-11 04:39:24 +0200

    suggestions

--- app/src/main/java/pl/edu/zut/mad/hackathon2016/MainActivity.java
+++ app/src/main/java/pl/edu/zut/mad/hackathon2016/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,7 +21,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
+        invalidateOptionsMenu();
         setSupportActionBar(toolbar);
     }
 
@@ -28,14 +31,6 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
-
-        SearchManager searchManager =
-                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView =
-                (SearchView) menu.findItem(R.id.action_search).getActionView();
-        searchView.setSearchableInfo(
-                searchManager.getSearchableInfo(getComponentName()));
-
 
         return true;
     }
--- app/src/main/java/pl/edu/zut/mad/hackathon2016/SearchActivity.java
+++ app/src/main/java/pl/edu/zut/mad/hackathon2016/SearchActivity.java
@@ -3,13 +3,20 @@ package pl.edu.zut.mad.hackathon2016;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
+import android.database.AbstractCursor;
+import android.database.Cursor;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
+import android.text.TextUtils;
 import android.view.Menu;
 
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Locale;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -23,7 +30,6 @@ public class SearchActivity extends AppCompatActivity {
             String query = intent.getStringExtra(SearchManager.QUERY);
             searchOrliks(query);
         }
-
     }
 
     @Override
@@ -47,6 +53,7 @@ public class SearchActivity extends AppCompatActivity {
                 return false;
             }
         });
+
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener()
         {
             @Override
@@ -63,7 +70,8 @@ public class SearchActivity extends AppCompatActivity {
                 return false;
             }
         });
-        return super.onPrepareOptionsMenu(menu) | true;
+
+        return true;
     }
 
     public static class SearchSuggestionsAdapter extends SimpleCursorAdapter
@@ -75,6 +83,96 @@ public class SearchActivity extends AppCompatActivity {
         public SearchSuggestionsAdapter(Context context)
         {
             super(context, android.R.layout.simple_list_item_1, null, mVisible, mViewIds, 0);
+        }
+
+        @Override
+        public Cursor runQueryOnBackgroundThread(CharSequence constraint)
+        {
+            return new SuggestionsCursor(constraint);
+        }
+
+        private static class SuggestionsCursor extends AbstractCursor
+        {
+            private ArrayList<String> mResults;
+
+            public SuggestionsCursor(CharSequence constraint)
+            {
+                final int count = 100;
+                mResults = new ArrayList<>(count);
+                for(int i = 0; i < count; i++){
+                    mResults.add("Result " + (i + 1));
+                }
+                if(!TextUtils.isEmpty(constraint)){
+                    String constraintString = constraint.toString().toLowerCase(Locale.ROOT);
+                    Iterator<String> iter = mResults.iterator();
+                    while(iter.hasNext()){
+                        if(!iter.next().toLowerCase(Locale.ROOT).startsWith(constraintString))
+                        {
+                            iter.remove();
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public int getCount()
+            {
+                return mResults.size();
+            }
+
+            @Override
+            public String[] getColumnNames()
+            {
+                return mFields;
+            }
+
+            @Override
+            public long getLong(int column)
+            {
+                if(column == 0){
+                    return mPos;
+                }
+                throw new UnsupportedOperationException("unimplemented");
+            }
+
+            @Override
+            public String getString(int column)
+            {
+                if(column == 1){
+                    return mResults.get(mPos);
+                }
+                throw new UnsupportedOperationException("unimplemented");
+            }
+
+            @Override
+            public short getShort(int column)
+            {
+                throw new UnsupportedOperationException("unimplemented");
+            }
+
+            @Override
+            public int getInt(int column)
+            {
+                throw new UnsupportedOperationException("unimplemented");
+            }
+
+            @Override
+            public float getFloat(int column)
+            {
+                throw new UnsupportedOperationException("unimplemented");
+            }
+
+            @Override
+            public double getDouble(int column)
+            {
+                throw new UnsupportedOperationException("unimplemented");
+            }
+
+            @Override
+            public boolean isNull(int column)
+            {
+                return false;
+            }
         }
     }
 
