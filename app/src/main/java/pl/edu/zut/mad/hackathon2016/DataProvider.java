9751d4cc785dfbcc8ad2ed7a1808ddc0490d65bb
Marek Macko <macko.marek93@gmail.com>
2016-06-11 09:20:41 +0200

    Added favourites orliks

---
+++ app/src/main/java/pl/edu/zut/mad/hackathon2016/DataProvider.java
@@ -0,0 +1,43 @@
+package pl.edu.zut.mad.hackathon2016;
+
+
+import android.support.annotation.NonNull;
+
+import com.raizlabs.android.dbflow.sql.language.Condition;
+import com.raizlabs.android.dbflow.sql.language.CursorResult;
+import com.raizlabs.android.dbflow.sql.language.Select;
+import com.raizlabs.android.dbflow.structure.database.transaction.QueryTransaction;
+
+import java.util.List;
+
+import pl.edu.zut.mad.hackathon2016.api.RequestCallback;
+import pl.edu.zut.mad.hackathon2016.api.RequestListener;
+import pl.edu.zut.mad.hackathon2016.api.RestClientManager;
+import pl.edu.zut.mad.hackathon2016.model.Orlik;
+import pl.edu.zut.mad.hackathon2016.model.Orlik_Table;
+
+public class DataProvider {
+
+    public static void getOrliks(final RequestListener<List<Orlik>> listener) {
+        new Select()
+                .from(Orlik.class)
+                .async()
+                .queryResultCallback(new QueryTransaction.QueryResultCallback<Orlik>() {
+                    @Override
+                    public void onQueryResult(QueryTransaction transaction, @NonNull CursorResult<Orlik> tResult) {
+                        if (tResult.toList() == null) {
+                            RestClientManager.getAllOrliks(new RequestCallback<>(listener));
+                        } else {
+                            listener.onSuccess(tResult.toList());
+                        }
+                    }
+                }).execute();
+    }
+
+    public static List<Orlik> getFavouritesOrliks() {
+        return new Select()
+                .from(Orlik.class)
+                .where(Condition.column(Orlik_Table.isFavourite.getNameAlias()).eq(true))
+                .queryList();
+    }
+}
